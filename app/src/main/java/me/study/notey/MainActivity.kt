c513762aff56c3a882072c54d372e4ce8ca0dca3
diegoalves <diegomartns94@gmail.com>
2024-02-15 17:28:32 -0300

    SplashScreen created;

--- app/src/main/java/me/study/notey/MainActivity.kt
+++ app/src/main/java/me/study/notey/MainActivity.kt
@@ -3,11 +3,13 @@ package me.study.notey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import me.study.notey.ui.theme.NoteyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NoteyTheme {
 
