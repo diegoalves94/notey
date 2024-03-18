88449a982e9f61f9c2cca6f13c4765906d7d7346
diegoalves <diegomartns94@gmail.com>
2024-03-18 17:39:35 -0300

    Setup Firebase and AppDistribution Workflow;

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
     id("io.realm.kotlin") version "1.11.0" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-//    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
