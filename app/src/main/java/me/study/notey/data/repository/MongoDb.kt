d9fc64e35450779bb7dcea6f33a2e75f2cf0db56
diegoalves <diegomartns94@gmail.com>
2024-04-11 19:33:33 -0300

    Section 7 - Home Screen: Padding Adjustements; Navigation with args; Write Screen: Created view; Implemented Insert, Update, Delete;

--- app/src/main/java/me/study/notey/data/repository/MongoDb.kt
+++ app/src/main/java/me/study/notey/data/repository/MongoDb.kt
@@ -13,6 +13,7 @@ import me.study.notey.models.Note
 import me.study.notey.util.Constants.APP_ID
 import me.study.notey.util.RequestState
 import me.study.notey.util.toInstant
+import org.mongodb.kbson.ObjectId
 import java.time.ZoneId
 
 object MongoDb : MongoRepository {
@@ -67,6 +68,96 @@ object MongoDb : MongoRepository {
             flow {
                 emit(RequestState.Error(UserNotAuthenticatedException()))
             }
+        }
+    }
+
+    override fun getSelectedNote(noteId: ObjectId): Flow<RequestState<Note>> {
+        return if (user != null) {
+            try {
+                realm.query<Note>(
+                    query = "_id == $0", noteId
+                ).asFlow().map {
+                    RequestState.Success(data = it.list.first())
+                }
+            } catch (e: Exception) {
+                flow {
+                    emit(RequestState.Error(e))
+                }
+            }
+        } else {
+            flow {
+                emit(RequestState.Error(UserNotAuthenticatedException()))
+            }
+        }
+    }
+
+    override suspend fun insertNote(note: Note): RequestState<Note> {
+        return if (user != null) {
+            realm.write {
+                try {
+                    val addedNote = copyToRealm(
+                        note.apply {
+                            ownerId = user.id
+                        }
+                    )
+                    RequestState.Success(addedNote)
+                } catch (e: Exception) {
+                    RequestState.Error(e)
+                }
+            }
+        } else {
+            RequestState.Error(UserNotAuthenticatedException())
+        }
+    }
+
+    override suspend fun updateNote(note: Note): RequestState<Note> {
+        return if (user != null) {
+            realm.write {
+                val queriedNote = query<Note>(
+                    query = "_id == $0", note._id
+                ).first().find()
+
+                if (queriedNote != null) {
+                    queriedNote.title = note.title
+                    queriedNote.description = note.description
+                    queriedNote.mood = note.mood
+                    queriedNote.images = note.images
+                    queriedNote.date = note.date
+
+                    RequestState.Success(
+                        data = queriedNote
+                    )
+                } else {
+                    RequestState.Error(
+                        error = Exception("This Note does not exist!")
+                    )
+                }
+            }
+        } else {
+            RequestState.Error(UserNotAuthenticatedException())
+        }
+    }
+
+    override suspend fun deleteNote(noteId: ObjectId): RequestState<Note> {
+        return if (user != null) {
+            realm.write {
+                val note = query<Note>(
+                    query = "_id == $0 AND ownerId == $1", noteId, user.id
+                ).first().find()
+                if (note != null) {
+                    try {
+
+                        delete(note)
+                        RequestState.Success(data = note)
+                    } catch (e: Exception) {
+                        RequestState.Error(e)
+                    }
+                }else{
+                    RequestState.Error(Exception("Note does note exist."))
+                }
+            }
+        } else {
+            RequestState.Error(UserNotAuthenticatedException())
         }
     }
 }
---
+++ app/src/main/java/me/study/notey/presentation/screens/write/WriteViewModel.kt
@@ -0,0 +1,165 @@
+package me.study.notey.presentation.screens.write
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import me.study.notey.data.repository.MongoDb
+import me.study.notey.models.Mood
+import me.study.notey.models.Note
+import me.study.notey.models.UiState
+import me.study.notey.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
+import me.study.notey.util.RequestState
+import me.study.notey.util.toRealmInstant
+import org.mongodb.kbson.ObjectId
+import java.time.ZonedDateTime
+
+class WriteViewModel(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    var uiState by mutableStateOf(UiState())
+        private set
+
+    init {
+        getNoteIdArg()
+        fetchSelectedNote()
+    }
+
+    private fun getNoteIdArg() {
+        uiState = uiState.copy(
+            selectedNoteId = savedStateHandle.get<String>(
+                key = WRITE_SCREEN_ARGUMENT_KEY
+            )
+        )
+    }
+
+    private fun fetchSelectedNote() {
+        if (uiState.selectedNoteId != null) {
+            viewModelScope.launch(Dispatchers.Main) {
+                MongoDb.getSelectedNote(
+                    noteId = ObjectId.invoke(uiState.selectedNoteId!!)
+                ).catch {
+                    emit(RequestState.Error(Exception("Note already deleted")))
+                }
+                    .collect { note ->
+                        if (note is RequestState.Success) {
+                            setSelectedNote(note = note.data)
+                            setTitle(title = note.data.title)
+                            setDescription(description = note.data.description)
+                            setMood(mood = Mood.valueOf(note.data.mood))
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setSelectedNote(note: Note) {
+        uiState = uiState.copy(
+            selectedNote = note
+        )
+    }
+
+    fun setTitle(title: String) {
+        uiState = uiState.copy(
+            title = title
+        )
+    }
+
+    fun setDescription(description: String) {
+        uiState = uiState.copy(
+            description = description
+        )
+    }
+
+    private fun setMood(mood: Mood) {
+        uiState = uiState.copy(
+            mood = mood
+        )
+    }
+
+    fun updateDateTime(zonedDateTime: ZonedDateTime) {
+        uiState = uiState.copy(updatedDateTime = zonedDateTime.toInstant().toRealmInstant())
+    }
+
+    fun upsertNote(note: Note, onSuccess: () -> Unit, onError: (String) -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (uiState.selectedNoteId != null) {
+                updateNote(
+                    note = note,
+                    onSuccess = onSuccess,
+                    onError = onError
+                )
+            } else {
+                insertNote(
+                    note = note,
+                    onSuccess = onSuccess,
+                    onError = onError
+                )
+            }
+        }
+    }
+
+    private suspend fun insertNote(note: Note, onSuccess: () -> Unit, onError: (String) -> Unit) {
+        val result = MongoDb.insertNote(note = note.apply {
+            if (uiState.updatedDateTime != null) {
+                date = uiState.updatedDateTime!!
+            }
+        })
+        if (result is RequestState.Success) {
+            withContext(Dispatchers.Main) {
+                onSuccess()
+            }
+        } else if (result is RequestState.Error) {
+            withContext(Dispatchers.Main) {
+                onError(result.error.message.toString())
+            }
+        }
+    }
+
+    private suspend fun updateNote(note: Note, onSuccess: () -> Unit, onError: (String) -> Unit) {
+        val result = MongoDb.updateNote(note.apply {
+            _id = ObjectId.invoke(uiState.selectedNoteId!!)
+            date = if (uiState.updatedDateTime != null) {
+                uiState.updatedDateTime!!
+            } else {
+                uiState.selectedNote!!.date
+            }
+        })
+        if (result is RequestState.Success) {
+            withContext(Dispatchers.Main) {
+                onSuccess()
+            }
+        } else if (result is RequestState.Error) {
+            withContext(Dispatchers.Main) {
+                onError(result.error.message.toString())
+            }
+        }
+    }
+
+    fun deleteNote(onSuccess: () -> Unit, onError: (String) -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (uiState.selectedNoteId != null) {
+                val result = MongoDb.deleteNote(
+                    noteId = ObjectId.invoke(uiState.selectedNoteId!!)
+                )
+                if (result is RequestState.Success) {
+                    withContext(Dispatchers.Main) {
+                        onSuccess()
+                    }
+                } else if (result is RequestState.Error) {
+                    withContext(Dispatchers.Main) {
+                        onError(result.error.message.toString())
+                    }
+                }
+
+            }
+        }
+    }
+}
